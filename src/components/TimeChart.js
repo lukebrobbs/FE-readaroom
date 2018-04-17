0193b0dd6b15cabd262699ea24b16d9c1177934c
SamiJG <samantha.gildart@gmail.com>
2018-04-17 22:09:33 +0100

    start writing function to calulate graph co-ords

--- src/components/TimeChart.js
+++ src/components/TimeChart.js
@@ -5,9 +5,9 @@ import db from '../config.js';
 class TimeChart extends React.Component {
   state = {
     options: {
-      title: 'Age vs. Weight comparison',
-      hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
-      vAxis: { title: 'Weight', minValue: 0, maxValue: 15 },
+      title: 'Audience Emotions over Time',
+      hAxis: { title: 'Time', minValue: 0 },
+      vAxis: { title: '% audience', minValue: 0, maxValue: 100 },
       legend: 'none'
     },
     rows: [[0, 0, 0, 0, 0, 0, 0, 0]],
@@ -16,31 +16,27 @@ class TimeChart extends React.Component {
       { type: 'number', label: 'Weight' }
     ]
   };
-  snapshot = db
-    .collection('sessions')
-    .doc('ZmrKU1MgAHfNFnqF2VJe')
-    .onSnapshot(function(doc) {
-      const emotions = [];
-      const emotionCount = {
-        HAPPY: 0,
-        SAD: 0,
-        ANGRY: 0,
-        CONFUSED: 0,
-        DISGUSTED: 0,
-        SURPRISED: 0,
-        CALM: 0
-      };
-      doc.data().students.forEach(student => {
-        emotions.push(student.emotions.slice(-3));
-      });
-      emotions.forEach(emotionData => {
-        emotionData.forEach(emotion => {
-          emotionCount[emotion.Type] += 1;
-        });
-      });
-    });
+  calculateGraphVectors = () => {
+    const whateverYouWannaCallIT = this.props.store
+      .getState()
+      .dataPoint.reduce((acc, curr) => {
+        for (let key in curr.emotion) {
+        }
 
+        acc[curr.timestamp] = {
+          HAPPY: 0,
+          SAD: 0,
+          ANGRY: 0,
+          CONFUSED: 0,
+          DISGUSTED: 0,
+          SURPRISED: 0,
+          CALM: 0
+        };
+        return acc;
+      }, {});
+  };
   render() {
+    this.calculateGraphVectors();
     return (
       <Chart
         chartType="LineChart"
