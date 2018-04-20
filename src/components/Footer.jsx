95c5810b22a4c0a1290fb01f282abbeff4f8b322
Chris Spence <christopherspenceuk@outlook.com>
2018-04-20 16:08:04 +0100

    Footer feature with app name and team names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const Footer = () => <p>Footer</p>;
+const Footer = () => (
+  <footer className="container-fluid bg-light mt-4">
+    <div className="container text-success py-2">
+      <p>Read A Room</p>
+      <p className="text-secondary">
+        Joe Berry, Luke Brobbin, Samantha Gildart & Chris Spence
+      </p>
+    </div>
+  </footer>
+);
 
 export default Footer;
