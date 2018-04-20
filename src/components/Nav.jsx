67755e67f8ecbd37751d87da93410712e2f7c444
Josephaberry54 <josephaberry54@gmail.com>
2018-04-20 11:44:34 +0100

    Creating a nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  state = {};
+
+  render() {
+    return (
+      <nav>
+        <h5>This is the NavBar</h5>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
