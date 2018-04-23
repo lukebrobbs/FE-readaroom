162db042187e284d61f46001fe665a2eaa592bec
Chris Spence <christopherspenceuk@outlook.com>
2018-04-23 14:38:05 +0100

    Header feature for readaroom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React from 'react';
+import logo from '../images/logo.png';
 
 const Header = () => (
-  <header>
-    <h1>Read-A-Room</h1>
+  <header className="container-fluid bg-dark py-4">
+    <div className="container justify-content-between">
+      <figure>
+        <img
+          className="d-inline mr-4 float-left"
+          src={logo}
+          height="100px"
+          width="100px"
+          alt="Read a room logo, a green square with rounded edges and two r characters"
+        />
+      </figure>
+      <h1 className="display-1 text-center text-success font-weight-bold d-inline">
+        Read a Room
+      </h1>
+    </div>
   </header>
 );
 
