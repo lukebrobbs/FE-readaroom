bc275e5dd86f9b795e6567914f2d45f76ca5c1c1
Chris Spence <christopherspenceuk@outlook.com>
2018-04-23 20:55:00 +0100

    moodboard animations

--- src/components/MoodBoard/MoodBoard.jsx
+++ src/components/MoodBoard/MoodBoard.jsx
@@ -6,18 +6,65 @@ class MoodBoard extends React.Component {
     return (
       <div>
         <h1>MoodBoard</h1>
+        <p>
+          Currently tracking {this.props.data.occupants || '0'} people in the
+          room
+        </p>
         <div className="container">
-          <div className="progress">
+          <div className="progress my-4" style={{ height: '50px' }}>
             <div
               className="progress-bar bg-success"
               role="progressbar"
-              style={{ width: this.props.happy }}
-            />
+              style={{ width: this.props.data.happy + '%' }}
+            >
+              <h3>Happy</h3>
+            </div>
             <div
               className="progress-bar bg-warning"
               role="progressbar"
-              style={{ width: this.props.sad }}
-            />
+              style={{ width: this.props.data.sad + '%' }}
+            >
+              <h3>Sad</h3>
+            </div>
+          </div>
+          <div className="progress my-4" style={{ height: '50px' }}>
+            <div
+              className="progress-bar bg-danger"
+              role="progressbar"
+              style={{ width: this.props.data.angry + '%' }}
+            >
+              <h3>Angry</h3>
+            </div>
+            <div
+              className="progress-bar bg-info"
+              role="progressbar"
+              style={{ width: this.props.data.calm + '%' }}
+            >
+              <h3>Calm</h3>
+            </div>
+          </div>
+          <div className="progress my-4" style={{ height: '50px' }}>
+            <div
+              className="progress-bar bg-info"
+              role="progressbar"
+              style={{ width: this.props.data.confused + '%' }}
+            >
+              <h3>Confused</h3>
+            </div>
+            <div
+              className="progress-bar bg-warning"
+              role="progressbar"
+              style={{ width: this.props.data.disgusted + '%' }}
+            >
+              <h3>Disgusted</h3>
+            </div>
+            <div
+              className="progress-bar bg-info"
+              role="progressbar"
+              style={{ width: this.props.data.surprised + '%' }}
+            >
+              <h3>Surprised</h3>
+            </div>
           </div>
         </div>
       </div>
--- src/components/MoodBoard/MoodBoardContainer.js
+++ src/components/MoodBoard/MoodBoardContainer.js
@@ -2,8 +2,49 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import MoodBoard from './MoodBoard';
 
+const processData = dataPoints => {
+  if (dataPoints.length) {
+    const emotions = {
+      HAPPY: 0,
+      SAD: 0,
+      ANGRY: 0,
+      CONFUSED: 0,
+      DISGUSTED: 0,
+      SURPRISED: 0,
+      CALM: 0
+    };
+    const emotionPercentages = [dataPoints[dataPoints.length - 1].timestamp];
+    dataPoints[dataPoints.length - 1].data.forEach(dataPoint => {
+      dataPoint.emotions.forEach(emotion => (emotions[emotion.Type] += 1));
+    });
+    const totalHS = emotions.HAPPY + emotions.SAD;
+    const totalAC = emotions.ANGRY + emotions.CALM;
+    const totalSDC =
+      emotions.SURPRISED + emotions.DISGUSTED + emotions.CONFUSED;
+    return {
+      occupants: dataPoints[dataPoints.length - 1].data.length,
+      happy: Math.round(emotions.HAPPY / totalHS) * 100,
+      sad: Math.round(emotions.SAD / totalHS) * 100,
+      angry: Math.round(emotions.ANGRY / totalAC) * 100,
+      calm: Math.round(emotions.CALM / totalAC) * 100,
+      surprised: Math.round(emotions.SURPRISED / totalSDC) * 100,
+      disgusted: Math.round(emotions.DISGUSTED / totalSDC) * 100,
+      confused: Math.round(emotions.CONFUSED / totalSDC) * 100
+    };
+  }
+  return {
+    happy: 50,
+    sad: 50,
+    angry: 50,
+    confused: 33,
+    disgusted: 33,
+    surprised: 34,
+    calm: 50
+  };
+};
+
 const mapStateToProps = (state, ownProps) => {
-  return { happy: '60%', sad: '40%' };
+  return { data: processData(state.dataPoints) };
 };
 const mapDispatchToProps = dispatch => {
   return {};
