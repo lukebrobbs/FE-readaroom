383ddeabb21e504a4a4a836fec053c5244f82555
Chris Spence <christopherspenceuk@outlook.com>
2018-04-23 14:41:56 +0100

    Footer dark bg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="container-fluid bg-light mt-4">
+  <footer className="container-fluid bg-dark mt-4">
     <div className="container text-success py-2">
       <p>Read A Room</p>
       <p className="text-secondary">
