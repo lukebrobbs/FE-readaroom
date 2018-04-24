fe7580d36218b173946d5928eed7040c34f6d361
Josephaberry54 <josephaberry54@gmail.com>
2018-04-24 11:06:46 +0100

    Placing Setup in a folder to group with Setup CSS file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import StartButtonContainer from './components/StartButton/StartButtonContainer';
 import Home from './components/Home/Home';
-import Setup from './components/Setup';
+import Setup from './components/Setup/Setup';
 import TimeChartContainer from './components/TimeChart/TimeChartContainer';
 import MoodBoardContainer from './components/MoodBoard/MoodBoardContainer';
 import SummaryContainer from './components/Summary/SummaryContainer';
