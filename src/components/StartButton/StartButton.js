3d3fc76f3b799a20eb7c2f3c9316be68f18e0802
Luke <lukemichaelvocalist@gmail.com>
2018-04-25 11:35:49 +0100

    add negate emotion function to firestore data

--- src/components/StartButton/StartButton.js
+++ src/components/StartButton/StartButton.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import db from '../../config/config.js';
+import React from "react";
+import db from "../../config/config.js";
 
-const sessionsRef = db.collection('readaroom').doc('currentData');
+const sessionsRef = db.collection("readaroom").doc("currentData");
 
 const StartButton = ({ onClick, addToDataPoint }) => {
   let time = 0;
   sessionsRef.onSnapshot(function(doc) {
     let now = time + 3;
     time += 3;
-    return addToDataPoint(time++, doc.data().emotiondata);
+    let docData = doc.data();
+    const filteredDocData = docData.emotiondata.map(data => {
+      let happyOrSadCount = 0;
+      let calmOrAngryCount = 0;
+      data.emotions = data.emotions.filter(emotion => {
+        if (emotion.Type === "HAPPY" || emotion.Type === "SAD") {
+          ++happyOrSadCount;
+          return happyOrSadCount === 1;
+        }
+        if (emotion.Type === "CALM" || emotion.Type === "ANGRY") {
+          ++calmOrAngryCount;
+          return calmOrAngryCount === 1;
+        } else return true;
+      });
+      return data;
+    });
+    return addToDataPoint(docData.timestamp, filteredDocData);
   });
   return (
     <button onClick={onClick} className="btn btn-success">
