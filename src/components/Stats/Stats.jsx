531df75c5611719f6a55f77b57f1e47e2ea380a9
Josephaberry54 <josephaberry54@gmail.com>
2018-04-26 11:06:52 +0100

    Minor change to Stats bootstrap

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 const Stats = ({ facesRecognised, averageAge, sessionDuration }) => {
   return (
-    <div className="container border bg-light">
+    <div className="container border bg-light my-4">
       <div className="row">
-        <div className="col">
+        <div className="col-4">
           <h2 className="text-right">Faces Recognised:</h2>
         </div>
-        <div className="col">
+        <div className="col-6">
           <h2>{facesRecognised}</h2>
         </div>
       </div>
       <div className="row">
-        <div>
-          <h2>Average Age:</h2>
+        <div className="col-4">
+          <h2 className="text-right">Average Age:</h2>
         </div>
-        <div>
+        <div className="col-6">
           <h2>{averageAge}</h2>
         </div>
       </div>
       <div className="row">
-        <div>
-          <h2>Session Duration:</h2>
+        <div className="col-4">
+          <h2 className="text-right">Session Duration:</h2>
         </div>
-        <div>
+        <div className="col-6">
           <h2>{sessionDuration}</h2>
         </div>
       </div>
