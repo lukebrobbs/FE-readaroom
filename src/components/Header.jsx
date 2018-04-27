bc0d1c6e03c310b305434c6330a6b8c12acf1fb2
SamiJG <samantha.gildart@gmail.com>
2018-04-27 09:56:18 +0100

    styling on header and homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import banner from "../images/RAR-header-light.png";
+import React from 'react';
+import banner from '../images/RAR-header-lighter.png';
 
 const Header = () => (
   <header>
     <figure>
       <img
-        // className="d-inline mr-4 float-left"
+        className="shadow-lg"
         src={banner}
         height="100%"
         width="100%"
